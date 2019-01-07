5d0c290fe54dee59405d675c5e51b7d74568b611
tommykjensen <tommykjensen@gmail.com>
2019-01-07 23:34:48 +0100

    Added Play Preset Station command

---
+++ DreamNucleus.Heos/Commands/Browse/PlayPresetStationCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DreamNucleus.Heos.Infrastructure.Heos;
+
+namespace DreamNucleus.Heos.Commands.Browse
+{
+    public sealed class PlayPresetStationCommand : Command<EmptyResponse>
+    {
+        public PlayPresetStationCommand(int playerId, int position)
+            : base($"browse/play_preset?pid={playerId}&preset={position}")
+        {
+
+        }
+
+        public override EmptyResponse Parse(Response response)
+        {
+            return new EmptyResponse();
+        }
+    }
+}
